0ae5e2d928dc74cbc05686a4480bf566b15642a4
webpods <robert@webpods.me>
2020-10-28 10:55:18 -0400

    update 15nice.sh

--- 15nice.sh
+++ 15nice.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# Conseev
-# Renice anything -15 to 0 besides killhighload
+# Renice anything -15 to 0 besides
 ps ax -o pid,ni,cmd | grep "\-15" | awk {'print $1'} | xargs renice 0
 ps ax -o pid,ni,cmd | grep "\-10" | awk {'print $1'} | xargs renice 0
 ps ax -o pid,ni,cmd | grep "\-20" | awk {'print $1'} | xargs renice 0
-renice -15 -p `cat /var/run/killhighload`;
 
