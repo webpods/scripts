e22552042c896d7973362861d18f5ed79003711f
webpods <robert@webpods.me>
2020-10-30 15:45:07 -0400

    test

--- 15nice.sh
+++ 15nice.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-## Renice anything -15 to 0 besides
+# Renice anything -15 to 0 besides
 ps ax -o pid,ni,cmd | grep "\-15" | awk {'print $1'} | xargs renice 0
 ps ax -o pid,ni,cmd | grep "\-10" | awk {'print $1'} | xargs renice 0
 ps ax -o pid,ni,cmd | grep "\-20" | awk {'print $1'} | xargs renice 0
