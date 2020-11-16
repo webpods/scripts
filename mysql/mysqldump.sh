2b3b624b807b6ea341332304b4b210d3d6c9f7fa
webpods <robert@webpods.me>
2020-11-16 15:37:50 -0500

    change mysqldump.sh

--- mysql/mysqldump.sh
+++ mysql/mysqldump.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
-#DIR=/opt/dumps
+#mySQL Dumper .01
 tstamp=`date +%s`
 
+	function mdump {
 
-function mdump {
-
-#echo "User to connect with/ leave blank for default)"
-#read user
-#echo "Password"
-#read pass
-
-for i in `mysql  -e "show databases"|grep -Ev "Database|information_schema|performance_schema"`
-        do echo Backing up $i
-        mysqldump  $i > $DIR/$i'_'$tstamp.sql
-        echo "Compressing backup of $i"
-        gzip -6 $DIR/$i'_'$tstamp.sql
-done
-        ls -larth $DIR/
+		#echo "User to connect with/ leave blank for default)"
+		#read user
+		#echo "Password"
+		#read pass
+		echo "Checking Dump directory" $DIR
+		mkdir -pv $DIR
+			for i in `mysql  -e "show databases"|grep -Ev "Database|information_schema|performance_schema"`
+			do echo Backing up $i
+			mysqldump  $i > $DIR/$i'_'$tstamp.sql
+        		echo "Compressing backup of $i"
+        		gzip -6 $DIR/$i'_'$tstamp.sql
+			done
+	        ls -larth $DIR/
 	}
 
 
-if [ -z $1 ]
+
+	if [ -z $1 ]
 	then
-	echo "Entering Interactive mode"
-	echo "Dump mySQL to what dir? Without the trailing /"
-	read DIR
+		echo "Entering Interactive mode"
+		echo "Dump mySQL to what dir? Without the trailing /"
+		read DIR
 
-	mdump
+		mdump
 	exit 	
-else
+	else
 
-DIR=$1
+		DIR=$1
+		echo "Checking Dump directory" $DIR
+		mkdir -pv $DIR
 
-echo "Non-Interactive Dunp to $DIR"
-	mdump
-	
+		echo "Non-Interactive Dunp to $DIR"
+		mdump
 
-fi
+	fi
 
 exit
 
