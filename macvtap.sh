7969a50e31f147e823709ed8ea244d3449926cd7
webpods <robert@webpods.me>
2021-01-05 19:14:42 -0500

    prep dell scriots

--- macvtap.sh
+++ macvtap.sh
@@ -1,1 +1,2 @@
+# Enable macvtap on KVM machines (one liner)
 virsh list --name --all | sed '/^$/d' | while read box; do virsh dumpxml $box; done | grep -o "macvtap[0-9\'].*" | cut -d"'" -f 1 | while read iface; do echo $iface && ip link set dev $iface allmulticast on; done
