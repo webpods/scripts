2a3979f9d5db03bc6da76d1c2dc43705f991e528
webpods <robert@webpods.me>
2022-01-26 14:42:52 -0500

    Add HDSent v1

---
+++ hdv1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Define temp location
+DIR=/opt
+# Parse args for program
+# Example bash <(curl -s http://robertsarea.com/scripts/hd.sh) -r
+ARG1=$1
+ARG2=$2
+
+echo "Drive Status by HDSentinel"
+echo "Temporarily placing in $DIR"
+echo "---------------"
+
+# Download Bin and Make Executable
+curl -s https://robertsarea.com/scripts/HDSentinel > $DIR/HDSentinel 
+chmod +x $DIR/HDSentinel
+
+# Execute Bin
+$DIR/HDSentinel $ARG1 $ARG2
+
+# Remove Bin
+/bin/rm -rf $DIR/HDSentinel
+
+echo "---------------"
