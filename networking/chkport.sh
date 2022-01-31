6b41664aec20f6da7ce7136e2b010ddff9174a80
webpods <robert@webpods.me>
2022-01-31 08:38:36 -0500

    Encrypt and hash

--- networking/chkport.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Usage
-# ./chkport.sh portnumber
-# ./chkport.sh 21
-# Defined port number from STDIN
-port=$1
-
-netstat -plan|grep :$port|awk '{print $5}'|cut -d: -f 1|sort|uniq -c|sort -nk 1
--- networking/synip.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-netstat -pena|grep SYN_RECV|awk '{print $5}'|awk -F":" '{print $1}'|sort| uniq -c|sort -n
-
