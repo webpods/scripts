e4d49681581af36b7c4c123cf92bff02b3dff400
webpods <robert@webpods.me>
2022-08-07 10:00:20 -0400

    checkip tweaks

--- checkip.sh
+++ checkip.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-# cyber@webpods.com
+#  CheckIP Against  AbuseIPDB
 
 KEY=<KEY>
-DAYS=<DAYS>
 IP=$1
+
+# Optional Max days, if nothing default to 5
+if [ "$2" == "" ]; then
+    DAYS=5
+else
+	DAYS=$2
+fi
+
 
 curl -s -G https://api.abuseipdb.com/api/v2/check \
   --data-urlencode "ipAddress=$IP" \
-  -d maxAgeInDays=7 \
+  -d maxAgeInDays=$DAYS \
   -d verbose \
   -H "Key: $KEY" \
   -H "Accept: application/json" | json_pp
