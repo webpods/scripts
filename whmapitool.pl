4df37c57be05f4f8a2b037e724e8c0817b419856
webpods <robert@webpods.me>
2022-11-21 18:04:20 -0500

    WHMAPITool - Swiss Army Knife of commands

---
+++ whmapitool.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+# github.com/webpods/scripts/
+# WHMAPI Tool
+# by Webpods, LCC
+# cyber@webpods.com
+#####################
+print "WHMAPI Tool";
+
+
+sub phpvhost {
+
+system("/usr/sbin/whmapi1 php_get_vhost_versions")
+
+	}
