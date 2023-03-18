7bfcbea114b1db0961046e26c733ee718a52106c
webpods <robert@webpods.me>
2023-03-18 10:15:36 -0400

    DNSCheck adjust

--- dnscheck.sh
+++ dnscheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# DNS Checker 1.1
+# DNS Checker 1.1.2
 # Will need to functionize it
 
 if [ "$1" == "" ]; then
@@ -18,6 +18,4 @@ whois --no-recursion $domain|grep -i 'name server'|head -n4|awk {'print $3'}
 echo " ---------------------------"
 #echo "DNS Checking against Cloudflare, Google, Quad9, OpenDNS"
 echo ""
-
-for i in $ns; do echo "Checking $domain against $i";echo "---------------------------";echo "";echo "Name Servers for $domain";echo ""; dig @$i NS $domain +short;echo "DNSSEC Check:";echo "";whois --no-recursion $1|grep "DNSSEC"|tr -d ' ' ;echo "--";echo "CAA records";echo ""; dig @$ns CAA $domain +short;echo "--";echo "A record for $domain";echo ""; dig @$i A $domain +short;echo "---";echo "MX records for $domain";echo ""; dig @$i MX $domain +short;echo "---";echo "TXT records for $domain"; dig @$i TXT $domain +short;dig @$ns TXT default._domainkey.$1 +short; dig @$ns TXT _dmarc.$1 +short;echo "---------------------------";echo "";done
-
+for i in $ns;do echo "Checking $domain against $i";echo "---------------------------";echo "";echo "Name Servers for $domain";echo "---";dig @$i NS $domain +short;echo "---";echo "DNSSEC Check:";echo "";whois --no-recursion $1|grep "DNSSEC"|tr -d ' ';echo "---";echo "CAA records";echo "";dig @$ns CAA $domain +short;echo "---";echo "A record for $domain";echo "";dig @$i A $domain +short;echo "---";echo "MX records for $domain";echo "";dig @$i MX $domain +short;echo "---";echo "MX IPs";echo "---";dig @$ns MX $1 +short|awk {'print $2'}|xargs dig A +short;echo "---";echo "TXT records for $domain
