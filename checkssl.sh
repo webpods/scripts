fb015c85478f9b5e885257ed91035f05192b034c
webpods <robert@webpods.me>
2023-05-04 18:23:21 -0400

    port sslshopper

--- checkssl.sh
+++ checkssl.sh
@@ -4,7 +4,7 @@
 # github.com/webpods/scripts/
 # SSL Checker
 echo "Usage:"
-echo "sslcheck domain.tld server.tld.com"
+echo "sslcheck domain.tld server/ip port"
 echo "---------"
 echo ""
 dom=$1
@@ -15,13 +15,24 @@ else
 	server=$2
 fi
 
+if [ "$3" == "" ]; then
+    port=443
+else
+	port=$3
+fi
+
 #dom2=www.$1
 let CertificateExpirationWarningTrigger=60*60*24
 echo "SSL Information for $dom:"
 echo " --------------------------- "
-echo|openssl s_client -servername $dom -connect $server:443 2>/dev/null|openssl x509 -noout -issuer -subject -ext subjectAltName -dates -fingerprint -serial
+echo|openssl s_client -servername $dom -connect $server:$port 2>/dev/null|openssl x509 -noout -issuer -subject -ext subjectAltName -dates -fingerprint -serial
 echo " --------------------------- "
-echo "CLI: echo|openssl s_client -servername $dom -connect $server:443 2>/dev/null|openssl x509 -noout -issuer -subject -ext subjectAltName -dates -fingerprint -serial"
-echo " "
-#echo "Checking with" `openssl version|awk {'print $1,$2'};`
+echo "CLI: echo|openssl s_client -servername $dom -connect $server:$port 2>/dev/null|openssl x509 -noout -issuer -subject -ext subjectAltName -dates -fingerprint -serial"
+echo "--"
+if [ "$port" == "443" ]; then
+	echo "https://www.sslshopper.com/ssl-checker.html#hostname=https://$dom"
+else
+	echo "https://www.sslshopper.com/ssl-checker.html#hostname=https://$dom:$port"
+
+fi
 
